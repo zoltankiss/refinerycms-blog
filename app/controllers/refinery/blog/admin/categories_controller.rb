227af9613712711964ef193e243659efb9f72138
Zoltan Kiss <zkiss@ackmanndickenson.com>
2014-10-05 22:28:57 -0500

    implement nested category trees feature

--- app/controllers/refinery/blog/admin/categories_controller.rb
+++ app/controllers/refinery/blog/admin/categories_controller.rb
@@ -6,6 +6,20 @@ module Refinery
         crudify :'refinery/blog/category',
                 :order => 'title ASC'
 
+        def create
+          permitted = params.require(:category).permit(:parent_id, :title)
+          if Category.create(permitted)
+            flash.notice = t(
+              'refinery.crudify.created',
+              :what => 'Category'
+            )
+
+            create_or_update_successful
+          else
+            create_or_update_unsuccessful 'edit'
+          end
+        end
+
         private
 
         def category_params
